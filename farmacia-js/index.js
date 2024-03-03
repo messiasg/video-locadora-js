0b015692965db406e984405183867347d2b74314
Messias <messiasguilherme700@gmail.com>
2024-03-03 10:22:12 -0300

    incluindo projeto farmacia JS

--- farmacia-js/index.js
+++ farmacia-js/index.js
@@ -1,17 +1,17 @@
 function calculoPromocao() {
   // Selecionar elementos digitados nos campos pelo usuário
-  const veiculo = document.getElementById("veiculo").value;
+  const medicamento = document.getElementById("medicamento").value;
   let preco = document.getElementById("preco");
   preco = Number(preco.value);
 
-  // Calculo da porcetagem e valor da parcela
-  const valorDeEntrada = preco / 2;
-  const valorPorParcela = valorDeEntrada / 12;
+  // Calculo das unidades e do desconto tirando os centavos
+  const valorDuasUnidades = preco * 2;
+  const valorComDesconto = Math.round(valorDuasUnidades)
 
   // Selecionar no DOM o campo para inclusão da resposta e texto inserido
   const resposta = document.getElementById("resposta");
   resposta.textContent =
-    "Promoção: " + veiculo + "\nEntrada de R$ " + valorDeEntrada.toFixed(2) + "\n12x de R$ " + valorPorParcela.toFixed(2);
+    "Promoção: " + medicamento + " Leve 2 por apenas R$ " + valorComDesconto.toFixed(2);
 }
 
 // Selecionar botão e adicionar evento
